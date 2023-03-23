54d3d9848e9de1fc1eea5bd4da51f1e2074bd13c
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-22 18:30:11 -0700

    prisma client hooked up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link"
 
 
+async function getPosts(){
+  const res = await fetch('/api/getPosts')
+}
+
+
+
 export default function Home() {
+
   return (
     <main className="py-4 px-48">
       <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md"
---
+++ pages/api/getPosts.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from '../../prisma/client'
+
+type Data = {
+    name: string
+}
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method === 'GET') {
+        try{
+            // Get prisma to festch the posts
+            const data = await prisma.post.findMany()
+        } catch(error) {
+
+        }
+    }
+}
