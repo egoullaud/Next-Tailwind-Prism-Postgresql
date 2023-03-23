963df97084778057bcb97bb0d09db11a50877eaa
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-22 18:58:36 -0700

    added prism function

--- pages/api/getPosts.ts
+++ pages/api/getPosts.ts
@@ -1,17 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../prisma/client";
 
-type Data = {
-  name: string;
-};
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   if (req.method === "GET") {
     try {
       // Get prisma to festch the posts
       const data = await prisma.post.findMany();
-    } catch (error) {}
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 }
