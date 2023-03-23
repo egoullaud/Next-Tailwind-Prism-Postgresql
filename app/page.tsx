1bb246008b841966eb9bc359e86294981da58b21
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-22 18:54:56 -0700

    formatted with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import Link from "next/link"
+import Link from "next/link";
 
-
-async function getPosts(){
-  const res = await fetch('/api/getPosts')
+async function getPosts() {
+  const res = await fetch("/api/getPosts");
 }
 
-
-
 export default function Home() {
-
   return (
     <main className="py-4 px-48">
-      <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md"
-       href='/dashboard'
-      >Go to the dashboard</Link>
+      <Link
+        className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md"
+        href="/dashboard"
+      >
+        Go to the dashboard
+      </Link>
     </main>
-  )
+  );
 }
--- pages/api/getPosts.ts
+++ pages/api/getPosts.ts
@@ -1,19 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import prisma from '../../prisma/client'
+import prisma from "../../prisma/client";
 
 type Data = {
-    name: string
-}
+  name: string;
+};
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
 ) {
-    if (req.method === 'GET') {
-        try{
-            // Get prisma to festch the posts
-            const data = await prisma.post.findMany()
-        } catch(error) {
-
-        }
-    }
+  if (req.method === "GET") {
+    try {
+      // Get prisma to festch the posts
+      const data = await prisma.post.findMany();
+    } catch (error) {}
+  }
 }
